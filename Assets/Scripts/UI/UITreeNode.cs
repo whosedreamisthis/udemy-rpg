50631df6fd65fa8c462cf603c4600f1b940f684a
Dana Sharon <whosedreamisthis@gmail.com>
2025-11-16 13:03:19 -0800

    UI Tree Node

---
+++ Assets/Scripts/UI/UITreeNode.cs
@@ -0,0 +1,86 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UITreeNode
+    : MonoBehaviour,
+        IPointerEnterHandler,
+        IPointerExitHandler,
+        IPointerDownHandler
+{
+    [SerializeField]
+    private UnityEngine.UI.Image skillIcon;
+
+    [SerializeField]
+    private string lockedColorHex = "#5E5C5C";
+    private Color lastColor;
+    public bool isUnlocked;
+    public bool isLocked;
+
+    private void Awake()
+    {
+        isLocked = false;
+        isUnlocked = false;
+        UpdateIconColor(GetColorByHex(lockedColorHex));
+    }
+
+    private void Unlock()
+    {
+        isUnlocked = true;
+        UpdateIconColor(Color.white);
+    }
+
+    private void UpdateIconColor(Color color)
+    {
+        if (skillIcon == null)
+        {
+            return;
+        }
+        lastColor = skillIcon.color;
+        skillIcon.color = color;
+    }
+
+    private bool CanBeUnlocked()
+    {
+        if (isLocked || isUnlocked)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (CanBeUnlocked())
+        {
+            Unlock();
+        }
+        else
+        {
+            Debug.Log("Cannot be unlocked");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!isUnlocked)
+        {
+            UpdateIconColor(Color.white * 0.9f);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isUnlocked)
+        {
+            UpdateIconColor(lastColor);
+        }
+    }
+
+    private Color GetColorByHex(string hexNumber)
+    {
+        ColorUtility.TryParseHtmlString(hexNumber, out Color color);
+        return color;
+    }
+}
